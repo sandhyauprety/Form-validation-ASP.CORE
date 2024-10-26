c5a38aa5fd2eacf6f72d28f37b72c500542b63b2
delll <delll@DESKTOP-FHD2Q16>
2024-10-26 11:29:19 +0545

    Add project files.

---
+++ Form validation/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using Form_validation.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Form_validation.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+       
+         public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public string Index(student std)
+        {
+            if(ModelState.IsValid)
+            {
+                return "Name is:" + std.Name+ "Surname is:" + std.Surname+ " Email is:" + std.Name+ "Age is:" + std.Age;
+
+
+            }
+            else
+            {
+                return "Validation Failed..";
+            }
+                
+            
+            
+        
+    
+        
+          
+    
+
+    }
+    public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Form validation/Models/student.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Form_validation.Models
+{
+    public class student
+    {
+        [Required (ErrorMessage ="Name is must")]
+        [StringLength(15,MinimumLength =3,ErrorMessage="Name must be in mimimum 3 to maximum 15 character")]
+        public string Name{ get; set; }
+
+        [Required(ErrorMessage ="Surname is must") ]
+        [StringLength(15, MinimumLength = 3, ErrorMessage = "Surname must be in mimimum 3 to maximum 15 character")]
+        public string Surname { get; set; }
+
+      
+
+        [EmailAddress(ErrorMessage ="Email is must")]
+        [StringLength(25, MinimumLength = 5, ErrorMessage = "Email must be in mimimum 5 to maximum 25 character")]
+        public string Email { get; set; }
+        
+
+
+        [Required (ErrorMessage ="Age should be in number")]
+        [StringLength(90, MinimumLength = 5, ErrorMessage = "Age must be in mimimum 5 to maximum 90 character")]
+
+        public int Age { get; set; }
+
+
+
+
+    }
+}
